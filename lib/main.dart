f5241c9e1c1f5ba4291a3c6b6cd0078ae7ea82ba
mohssineAboutaj <mohssineaboutaj1995@gmail.com>
2020-11-02 16:09:26 +0100

    update btn text

--- lib/main.dart
+++ lib/main.dart
@@ -1,123 +1,122 @@
 import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-	home: Home(),
-));
+      home: Home(),
+    ));
 
 class Home extends StatelessWidget {
-	@override
-	Widget build(BuildContext context) {
-		Color primary = Colors.red[700];
-		const mainFontSize = 20.0;
+  @override
+  Widget build(BuildContext context) {
+    Color primary = Colors.red[700];
+    const mainFontSize = 20.0;
 
-		var userInfo = (args) {
-			return Row(
-				mainAxisAlignment: MainAxisAlignment.start,
-				children: <Widget>[
-					Padding(
-						padding: EdgeInsets.all(10),
-						child: Icon(
-							args["icon"],
-							color: primary,
-							size: 30.0,
-						),
-					),
-					Padding(
-						padding: EdgeInsets.all(10),
-						child: Text(
-							args["name"],
-							style: TextStyle(
-								fontSize: 20,
-							),
-						),
-					),
-				],
-			);
-		};
+    var userInfo = (args) {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Icon(
+              args["icon"],
+              color: primary,
+              size: 30.0,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Text(
+              args["name"],
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+          ),
+        ],
+      );
+    };
 
-		return Scaffold(
-			backgroundColor: Colors.grey[100],
-			appBar: AppBar(
-				title: Text(
-					'User Card',
-					style: TextStyle(
-						fontSize: mainFontSize * 1.5,
-					),
-				),
-				backgroundColor: primary,
-				centerTitle: true,
-			),
-			body: Column(
-				children: <Widget>[
-					Padding(
-						padding: EdgeInsets.fromLTRB(20, 20, 20, 40),
-						child: Center(
-							child: CircleAvatar(
-								backgroundImage: AssetImage('assets/imgs/user.jpg'),
-								radius: 60.0,
-							),
-						),
-					),
-					Divider(
-						height: 10.0,
-						color: Colors.grey[250],							
-					),
-					Padding(
-						padding: EdgeInsets.all(10),
-						child: Column(
-							children: <Widget>[
-								userInfo({
-									"name": "Mohssine",
-									"icon": Icons.person,
-								}),
-								userInfo({
-									"name": "mohssine@mohssine.ma",
-									"icon": Icons.email,
-								}),
-								userInfo({
-									"name": "0123456789",
-									"icon": Icons.phone,
-								}),
-								userInfo({
-									"name": "Web Developer",
-									"icon": Icons.work,
-								}),
-								userInfo({
-									"name": "123, x street, x city",
-									"icon": Icons.location_on,
-								}),
-								SizedBox(
-									height: 15,
-								),
-								RaisedButton(
-									color: Colors.red[500],
-									onPressed: () {},
-									child: Row(
-										children: <Widget>[
-											Padding(
-												padding: EdgeInsets.all(10),
-												child: Icon(
-													Icons.send,
-													color: Colors.white,
-												),
-											),
-											Padding(
-												padding: EdgeInsets.all(10),
-												child: Text('Send Mssage', style: TextStyle(
-													color: Colors.white,
-												)),
-											),
-										],
-									),
-								)
-							],
-						),
-					),					
-				],
-			),
-		);
-	}
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        title: Text(
+          'User Card',
+          style: TextStyle(
+            fontSize: mainFontSize * 1.5,
+          ),
+        ),
+        backgroundColor: primary,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.fromLTRB(20, 20, 20, 40),
+            child: Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/imgs/user.jpg'),
+                radius: 60.0,
+              ),
+            ),
+          ),
+          Divider(
+            height: 10.0,
+            color: Colors.grey[250],
+          ),
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              children: <Widget>[
+                userInfo({
+                  "name": "Mohssine",
+                  "icon": Icons.person,
+                }),
+                userInfo({
+                  "name": "mohssine@mohssine.ma",
+                  "icon": Icons.email,
+                }),
+                userInfo({
+                  "name": "0123456789",
+                  "icon": Icons.phone,
+                }),
+                userInfo({
+                  "name": "Web Developer",
+                  "icon": Icons.work,
+                }),
+                userInfo({
+                  "name": "123, x street, x city",
+                  "icon": Icons.location_on,
+                }),
+                SizedBox(
+                  height: 15,
+                ),
+                RaisedButton(
+                  color: Colors.red[500],
+                  onPressed: () {},
+                  child: Row(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Icon(
+                          Icons.send,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Text('Send A Message',
+                            style: TextStyle(
+                              color: Colors.white,
+                            )),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
-
